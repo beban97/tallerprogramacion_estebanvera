e96a2bf8460f40eb1faf20ba76ba5e569a2d233e
beban97 <evha90094745@gmail.com>
2023-08-31 17:38:12 -0400

    se agrega el proyecto

---
+++ README.md
@@ -0,0 +1,1 @@
+# tallerprogramacion_estebanvera
---
+++ ejercicio2/src/main/java/org/example/EmpleadoIndefinido.java
@@ -0,0 +1,22 @@
+package org.example;
+
+private class EmpleadoIndefinido extends Empleado{
+
+    public EmpleadoIndefinido(boolean hasvacations){
+        Super.getTiempoContrato();
+        if(super.getTiempoContrato() > 6){
+            super.SetHasVacations(true);
+        }
+    }
+
+    public void pagarFiniquito(int salario, int timepoContrato){
+        if(super.GetTimepoContrato() >= 12 ){
+           super.SetSalario = Super.GetSalario + 50000
+        }
+    }
+    public void darVacaciones(boolean hasVacations){
+        if(hasVacations = true){
+
+        }
+    }
+}
